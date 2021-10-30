2a69f34d18fbe3d13b953b5f2406ef13c2552a77
Nick Phair <nickphair@gmail.com>
2021-10-29 23:23:29 -0400

    make default font libre-franklin
    UVA recommends Franklin Gothic but you need a license
    for that. Libre-Franklin is an open-source alternative.

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -6,5 +6,8 @@ import 'home/home.dart';
 /// A [MaterialApp] which sets the `home` to [CounterPage].
 /// {@endtemplate}
 class MoralDistressApp extends MaterialApp {
+  @override
+  ThemeData? get theme => ThemeData(fontFamily: 'LibreFranklin');
+
   const MoralDistressApp({Key? key}) : super(key: key, home: const HomeRoute());
 }
