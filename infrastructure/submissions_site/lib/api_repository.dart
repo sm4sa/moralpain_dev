de0c34d5f1e7cc34954a08af499cc9735b7ef65c
Amanda Hauer <arh4uwe@virginia.edu>
2022-07-20 20:19:53 +0000

    Removed unnecessary loggers, methods, and error classes from API repositories in submissions site and analytics site

--- infrastructure/submissions_site/lib/api_repository.dart
+++ infrastructure/submissions_site/lib/api_repository.dart
@@ -1,5 +1,4 @@
 import 'dart:async' show Future;
-import 'package:logging/logging.dart';
 import 'package:moralpainapi/moralpainapi.dart';
 
 class SubmissionsFetchFailure implements Exception {
@@ -15,30 +14,7 @@ class SubmissionsFetchFailure implements Exception {
   }
 }
 
-class AnalyticsFetchFailure implements Exception {
-  const AnalyticsFetchFailure([
-    this.message = 'An unknown exception occurred.',
-  ]);
-
-  final String message;
-
-  @override
-  String toString() {
-    return 'AnalyticsFetchFailure($message)';
-  }
-}
-
-class SurveyFetchFailure implements Exception {
-  const SurveyFetchFailure([
-    this.message = 'An unknown exception occurred.',
-  ]);
-
-  final String message;
-}
-
 class ApiRepository {
-  final log = Logger('AdminRepository');
-
   late Moralpainapi mapi;
 
   ApiRepository(
@@ -68,35 +44,6 @@ class ApiRepository {
           .data!;
     } catch (err) {
       throw SubmissionsFetchFailure(err.toString());
-    }
-  }
-
-  Future<AnalyticsResult> fetchAnalytics({
-    required String operation,
-    int? starttime,
-    int? endtime,
-  }) async {
-    final aapi = mapi.getAdminApi();
-
-    try {
-      var response = await aapi.getAnalytics(
-        operation: operation,
-        starttime: starttime,
-        endtime: endtime,
-      );
-      return response.data!;
-    } catch (err) {
-      throw AnalyticsFetchFailure(err.toString());
-    }
-  }
-
-  Future<Survey> fetchSurvey() async {
-    final uapi = mapi.getUserApi();
-
-    try {
-      return (await uapi.getSurvey()).data!;
-    } catch (err) {
-      throw (SurveyFetchFailure(err.toString()));
     }
   }
 }
