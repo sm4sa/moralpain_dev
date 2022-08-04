d34ebdc6af4edde097fd8ea8eaf308bc0c6b966d
arh4uwe <arh4uwe@virginia.edu>
2022-08-04 00:44:58 +0000

    Added optional repository parameter to HomeRoute so that a mock repository can be passed in during testing

--- infrastructure/submission_site/lib/home/view/home_route.dart
+++ infrastructure/submission_site/lib/home/view/home_route.dart
@@ -4,13 +4,15 @@ import 'package:submission_site/api_repository.dart';
 import 'package:submission_site/home/home.dart';
 
 class HomeRoute extends StatelessWidget {
-  const HomeRoute({Key? key}) : super(key: key);
+  final ApiRepository? repository;
+
+  const HomeRoute({this.repository, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider<HomeBloc>(
       create: (_) => HomeBloc(
-        repository: ApiRepository(),
+        repository: repository ?? ApiRepository(),
       )..add(const HomeSubmissionRequested(
           '119ada26-a0ba-4991-82f4-d6aa7c73c503',
         )),
