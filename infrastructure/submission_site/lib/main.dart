7b2c29c7cb75a26881ed3a3ccfb326f1a15f937d
arh4uwe <arh4uwe@virginia.edu>
2022-08-05 00:13:26 +0000

    Removed unnecessary imports

--- infrastructure/submission_site/lib/main.dart
+++ infrastructure/submission_site/lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:moralpainapi/moralpainapi.dart';
 import 'package:submission_site/constants.dart';
 import 'package:submission_site/home/home.dart';
 
